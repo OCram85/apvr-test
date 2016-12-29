3d52d9df1170a65cbf0ea5b516c877d3c82bdd92
OCram85 <marco.blessing@googlemail.com>
2016-12-29 11:12:59 +0100

    wip

--- tools/AppVeyor.psm1
+++ tools/AppVeyor.psm1
@@ -6,10 +6,10 @@ Function Invoke-AppVeyorTests() {
     If (Test-Path -Path $testResultsFile) {
         Remove-Item -Path $testResultsFile -Force
     }
- 
+
     $res = Invoke-Pester -Path ".\tests\*" -OutputFormat NUnitXml -OutputFile $testResultsFile -PassThru
     (New-Object 'System.Net.WebClient').UploadFile("https://ci.appveyor.com/api/testresults/nunit/$($env:APPVEYOR_JOB_ID)", (Resolve-Path $testResultsFile))
-    If ($res.FailedCount -gt 0) { 
+    If ($res.FailedCount -gt 0) {
         Throw "$($res.FailedCount) tests failed."
     }
 
@@ -18,26 +18,7 @@ Function Invoke-AppVeyorTests() {
 Function Invoke-AppVeyorBuild() {
     [CmdletBinding()]
     Param()
-    if ($LastExitCode -eq 0) {
         #Get-ChildItem -Path ".\src\*" -Recurse | New-ZipFile -Path 'testbuild.zip'
         7z a myapp.zip ("{0}\src\*" -f $env:APPVEYOR_BUILD_FOLDER)
         Push-AppveyorArtifact myapp.zip
-    }
-    Else {
-        Add-AppveyorCompilationMessage "Build skipped by failed unit tests! " -Category Warning 
-    }
 }
-
-function Invoke-AppVeyorAfterTest() {
-    [CmdletBinding()]
-    Param()
-    $PSVersionTable
-}
-
-function Invoke-AppveyorFinish () {
-    [CmdletBinding()]
-    Param()
-    Write-Host ("PSScriptRoot: {0}" -f $PSScriptRoot )
-    Write-Host ("PSCmdlet: {0}" -f $PSCmdlet.MyInvocation.PSScriptRoot)
-}
-#Push-AppveyorArtifact <file_name>
